06df907e4678967ca5ab841f295fe063f38cb8b3
Dragonfly929 <zaica.maia3@gmail.com>
2023-09-28 23:07:15 +0300

    commit

--- homework.py
+++ homework.py
@@ -6,7 +6,6 @@ from concurrent.futures import ThreadPoolExecutor
 from in_class import parsed_product_urls
 
 
-# Function to extract product details from a product URL
 def extract_product_details(url):
     try:
         response = requests.get(url)
@@ -43,8 +42,10 @@ def extract_product_details(url):
             # Extract Price
             price_element = soup.find('li', class_='adPage__content__price-feature__prices__price is-main')
             if price_element:
-                currency_element = price_element.find('span', class_='adPage__content__price-feature__prices__price__currency')
-                value_element = price_element.find('span', class_='adPage__content__price-feature__prices__price__value')
+                currency_element = price_element.find('span',
+                                                      class_='adPage__content__price-feature__prices__price__currency')
+                value_element = price_element.find('span',
+                                                   class_='adPage__content__price-feature__prices__price__value')
                 if currency_element and value_element:
                     currency = currency_element.text.strip()
                     value = value_element.text.strip()
@@ -56,32 +57,46 @@ def extract_product_details(url):
         print(f"Error while processing {url}: {e}")
         return None
 
-# Function to save product details to a JSON file
+
 def save_product_details_to_json(product_details, filename):
     with open(filename, 'w', encoding='utf-8') as json_file:
         json.dump(product_details, json_file, ensure_ascii=False, indent=4)
 
-# List to store product details
+
 product_details_list = []
 
-# Function to scrape product details for a batch of URLs
 def scrape_batch_of_urls(urls):
     for url in urls:
         product_details = extract_product_details(url)
         if product_details:
             product_details_list.append(product_details)
 
-# Number of threads for concurrent scraping (adjust as needed)
+
 num_threads = 4
 
-# Split the URLs into batches for concurrent processing
 url_batches = [parsed_product_urls[i:i + num_threads] for i in range(0, len(parsed_product_urls), num_threads)]
 
-# Create a ThreadPoolExecutor for concurrent scraping
 with ThreadPoolExecutor(max_workers=num_threads) as executor:
     executor.map(scrape_batch_of_urls, url_batches)
 
-# Save the extracted product details to a JSON file
 save_product_details_to_json(product_details_list, "product_details.json")
 
 print("Scraping completed and data saved to product_details.json")
+
+
+
+# using the json file that includes the urls
+#
+# with open("parsed_product_urls.json", 'r') as json_file:
+#     product_urls = json.load(json_file)
+#
+#
+# product_details_list = []
+# for url in product_urls:
+#     product_details = extract_product_details(url)
+#     if product_details:
+#         product_details_list.append(product_details)
+#
+#
+# with open("product_details.json", 'w', encoding='utf-8') as json_file:
+#     json.dump(product_details_list, json_file, ensure_ascii=False, indent=4)
--- in_class.py
+++ in_class.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
 import json
 
-# Function to extract the product type
+
 def extract_product_type(start_url):
     parsed_url = urlparse(start_url)
     path_parts = parsed_url.path.split('/')
@@ -14,11 +14,11 @@ def extract_product_type(start_url):
             break
     return product_type
 
-# Function to determine if a URL is a booster
+
 def is_booster(link):
     return 'booster' not in link.lower()
 
-# Function to crawl product URLs and extract individual product URLs
+
 def crawl_product_urls(start_url, max_pages, parsed_product_urls=None):
     if parsed_product_urls is None:
         parsed_product_urls = []
@@ -29,7 +29,7 @@ def crawl_product_urls(start_url, max_pages, parsed_product_urls=None):
         print("Product type not found in the URL.")
         return parsed_product_urls
 
-    parsed_pages = set()  # Set to store visited page URLs
+    parsed_pages = set()
 
     page_number = 1
 
@@ -41,48 +41,44 @@ def crawl_product_urls(start_url, max_pages, parsed_product_urls=None):
             if response.status_code == 200:
                 soup = BeautifulSoup(response.text, 'html.parser')
 
-                # Extract product links and filter out boosters
                 product_links = soup.find_all('a', class_='js-item-ad', href=True)
                 for link in product_links:
                     absolute_path = urljoin(current_url, link['href'])
                     if is_booster(absolute_path):
                         parsed_product_urls.append(absolute_path)
 
-                parsed_pages.add(current_url)  # Add the parsed page URL to the set
+                parsed_pages.add(current_url)
 
                 page_number += 1
 
             else:
                 print(f"Error while processing {current_url}: {response.status_code}")
-                break  # Exit the loop if there's an error
+                break
 
         except Exception as e:
             print(f"Error while processing {current_url}: {e}")
-            break  # Exit the loop if there's an error
+            break
 
-    return parsed_product_urls, parsed_pages  # Return both the product URLs and parsed page URLs
+    return parsed_product_urls, parsed_pages
 
-# Function to save parsed product URLs to a JSON file
+
 def save_to_json(parsed_product_urls, filename):
     with open(filename, 'w') as json_file:
         json.dump(parsed_product_urls, json_file, ensure_ascii=False, indent=4)
 
-# Example usage with a single start URL:
-start_url = "https://999.md/ro/list/musical-instruments/guitars"
-max_pages = 1  # Adjust the maximum number of pages to crawl as needed
 
-# List to store the URLs of individual products
+start_url = "https://999.md/ro/list/musical-instruments/guitars"
+max_pages = 1  # adjust the maximum number of pages to crawl as needed
+
 parsed_product_urls, parsed_pages = crawl_product_urls(start_url, max_pages)
 
-# Save the extracted product URLs to a JSON file
 save_to_json(parsed_product_urls, "parsed_product_urls.json")
 
-# Print the extracted product URLs
+
 print("Extracted Product URLs:")
 for url in parsed_product_urls:
     print(url)
 
-# Print the URLs of the pages that were parsed
 print("\nParsed Page URLs:")
 for page_url in parsed_pages:
     print(page_url)
